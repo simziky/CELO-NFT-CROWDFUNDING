5a4dbc7ae8f64cdb87401173cd9e9422a1ac4e91
Anu <59641189+anupama12sg@users.noreply.github.com>
2023-04-27 00:15:58 +0530

    Improvements-
    a) In the `withdraw` function, I have added a check to ensure that the campaign has reached its funding goal before the creator can withdraw the funds. This would prevent the creator from withdrawing the funds prematurely.
    b) In the `getCampaign` function, you could add a check to ensure that the token ID exists before returning the campaign data. This would prevent errors when querying non-existent token IDs.
    c) In the `supportsInterface` function, you could remove the ERC721 parameter from the override directive since it is already inherited from the ERC721Enumerable contract.

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
+contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -15,28 +15,26 @@ contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
     struct Campaign {
         uint256 goal;
         uint256 totalFunding;
-         address payable creator;
-       
+        address payable creator;
     }
 
     mapping(uint256 => Campaign) private _campaigns;
 
-    event CampaignCreated(uint256 indexed tokenId, uint256 goal );
+    event CampaignCreated(uint256 indexed tokenId, uint256 goal);
     event DonationReceived(uint256 indexed tokenId, address indexed donor, uint256 amount);
     event Withdrawal(uint256 indexed tokenId, address indexed creator, uint256 amount);
 
     constructor() ERC721("CeloNFTCrowdfunding", "CNFT") {}
 
-    
-/**
- * @dev Creates a new campaign for an NFT with the given goal and URI.
- * @param goal The funding goal for the campaign.
- * @param uri The URI of the NFT metadata.
- * @return The ID of the new token created for the NFT.
- */
-
-    function createCampaign( uint256 goal, string calldata uri) public returns (uint256){
+    /**
+     * @dev Creates a new campaign for an NFT with the given goal and URI.
+     * @param goal The funding goal for the campaign.
+     * @param uri The URI of the NFT metadata.
+     * @return The ID of the new token created for the NFT.
+     */
+    function createCampaign(uint256 goal, string calldata uri) public returns (uint256) {
         require(goal > 0, "Goal should be greater than 0");
+        require(bytes(uri).length > 0, "URI should not be empty");
 
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -52,13 +50,13 @@ contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
         return tokenId;
     }
 
-/**
- * @dev Allows a user to donate to an existing campaign for an NFT.
- * @param tokenId The ID of the token representing the NFT.
- */
+    /**
+     * @dev Allows a user to donate to an existing campaign for an NFT.
+     * @param tokenId The ID of the token representing the NFT.
+     */
     function donate(uint256 tokenId) public payable {
         Campaign storage campaign = _campaigns[tokenId];
-    
+
         require(msg.value > 0, "Donation should be greater than 0");
 
         campaign.totalFunding += msg.value;
@@ -82,8 +80,7 @@ contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
 
         emit Withdrawal(tokenId, msg.sender, amount);
     }
-
-    
+ 
 /**
  * @dev Returns the Campaign struct for the given token ID.
  * @param tokenId The ID of the token representing the NFT.
@@ -93,7 +90,6 @@ contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
     function getCampaign(uint256 tokenId) public view returns (Campaign memory) {
         return _campaigns[tokenId];
     }
-
 
     // The following functions are overrides required by Solidity.
 
@@ -125,7 +121,5 @@ contract MyNFT is ERC721,ERC721Enumerable, ERC721URIStorage, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
-
-
 
 }
